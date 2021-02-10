2e95e037629729ee3f1861bfedbd796f12b55e99
31iSande <1140076527@qq.com>
2021-02-10 15:46:07 +0000

    Ver. 0.1.1 Add README w/ content script prototype

---
+++ README.md
@@ -0,0 +1,3 @@
+# TBD
+
+2020/0 MSci Project (CS5199): An Aid To Learning & Assessing To Read Foreign Language.
--- translator/contentScript.js
+++ translator/contentScript.js
@@ -1,8 +1,39 @@
 console.log('ContentScript installed');
 
-// Trigger: mouse click releasing
-document.onmouseup = function() {
+// MouseUp event
+document.onmouseup = function(e) {
+    // readSelection();
+    throttle(readSelection, 50);
+}
+
+/**
+ * Read the selected text
+ */
+let readSelection = function() {
     let _text = window.getSelection();
 
-    console.log(_text);
+    console.log(_text.toString());
+}
+
+/**
+ * Throttle: execute given function only once in the given delay duration
+ * @param {function} f          function to be throttled
+ * @param {number} delay        delay duration
+ */
+let throttle = function(f, delay) {
+    let _prev = 0;
+
+    console.log(_prev);
+
+    // Closure
+    return function() {
+        let _now = new Date();
+
+        console.log(_now - _prev);
+
+        if(_now - _prev > delay) {
+            f();
+            _prev = _now;
+        }
+    }();
 }
